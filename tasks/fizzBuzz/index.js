ef27cebda9aa7a54a6e65e9ba9d1d2d0446a4b90
Mindaugas-Kasiulis <mindaugas@kafene.com>
2024-12-03 06:12:05 +0200

    stuff

---
+++ tasks/fizzBuzz/index.js
@@ -0,0 +1,12 @@
+function fizzBuzz(n) {
+    // TODO: Implement the function
+}
+
+fizzBuzz(15);
+// Output: 1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz'
+
+
+
+//bonus points
+//fizzBuzz(15, {4: 'Quad'})
+// Output: 1, 2, 'Fizz', 'Quad', 'Buzz', 'Fizz', 7, Quad, 'Fizz', 'Buzz', 11, 'FizzQuad', 13, 14, 'FizzBuzzQuad'
---
+++ tasks/wordCount/index.js
@@ -0,0 +1,11 @@
+function wordCount(text) {
+    // TODO: Implement the function
+}
+
+wordCount('Hello, world!');
+//Output: { 'hello': 2, 'world': 1 }
+
+//bonus points
+//- Add a feature to exclude user-specified stop words from the count.
+//- Calculate the average word length in the text.
+//- Highlight the top N most frequent words in the results.
